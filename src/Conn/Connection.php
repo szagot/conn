c5837ce89cae16db8bcee98c05899cee7d1a2ac0
Daniel Bispo <daniel.bispo@datacoress.com>
2026-03-22 17:13:31 -0300

    fix: fecha a conexão com o banco

--- src/Conn/Connection.php
+++ src/Conn/Connection.php
@@ -60,8 +60,7 @@ class Connection
                         $mysqlInitCommand            => 'SET NAMES UTF8',
                         // Recepciona os erros com PDOException
                         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                        // Mantém aberta a Conexão com o Banco de Dados, se possível
-                        PDO::ATTR_PERSISTENT         => true,
+                        PDO::ATTR_PERSISTENT         => false,
                     ]);
             }
 
